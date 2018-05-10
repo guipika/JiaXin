956bdd04c96a8e0e4f1014a33a0aa447eb8e8c0d
feixiang <805818574@qq.com>
2018-05-10 11:58:55 +0800

    feixing peng
    新建Maven项目
    导入使用Maven导入
    JDK1.7.0_80

---
+++ troops/src/main/java/com/whwj/entity/Junxian.java
@@ -0,0 +1,12 @@
+package com.whwj.entity;
+
+/**
+ * 军衔
+ * @（类名）ClassName   : Junxian
+ * @（作者）Author      : feixiang Peng
+ * @（时间）Date        : 2018-5-10 上午11:52:52
+ * @（描述）Description :
+ */
+public class Junxian {
+	
+}
